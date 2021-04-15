87246b600e0180a4f4a83d2f8da218dbe3bfd962
LeoAdL <laparisidelannoy@uchicago.edu>
2021-04-15 18:11:57 -0500

    new

--- HW1.jl
+++ HW1.jl
@@ -1,4 +1,5 @@
 using DifferentialEquations
+using BoundaryValueDiffEq
 using LinearAlgebra, Statistics, Plots
 using Parameters,LaTeXStrings
 using NLsolve
@@ -7,24 +8,53 @@ using Distributions
 using Formatting
 using Printf
 
+#Schaab + KMV 2018
 
-function define_env(;ρ̄      =  .051,
-                     θᵨ     =   ,
-                     θᵢ      =   ,
+
+function define_env(;ρ̄      =  .022,
+                     θᵨ     =   .22,
+                     θᵢ      =   1.0,
+                     θ      =   100.0,
                      ϵ      =   10,
-                     ψ      =   1.0,
+                     ψ      =   2.0,
                      ϕ      =   1.25,
-                     γ      =   1.0)
+                     γ      =   2.0)
     σ   =   1/γ
     params  =   @with_kw (ρ̄      =  ρ̄,
                      θᵨ     =   θᵨ,
                      θᵢ      =   θᵢ,
+                     θ      =   θ,
                      ϵ      =   ϵ,
                      ψ      =   ψ,
                      ϕ      =   ϕ,
                      σ      =   σ,
-                     γ      =   γ)
+                     γ      =   γ,
+                     T      =   10.0,
+                     dt     =   .01)
     return params
 end
 
 params  =   define_env()
+
+function NK_Rote!(du,u,p,t)
+    @unpack σ,ϵ,θ,σ,ϕ,ψ,ρ̄,θᵨ,θᵢ =   params
+    x    =   u[1]
+    dx   =   du[1]
+
+    π   =   u[2]
+    dπ  =   du[2]
+
+    i   =   u[3]
+    di  =   du[3]
+
+    ρ   =   u[4]
+    dρ  =   du[4]
+
+    dx  =   σ*(i-π-ρ)*x
+    
+    dπ  =   (ϵ-1)/θ*(x^(1\σ+ψ)-1)-π*((1-σ)*(i-π)+σ*ρ)
+
+    di  =   -θᵢ*(i-ϕ*π)
+
+    dρ  =   -θᵨ*(ρ-ρ̄)
+end
