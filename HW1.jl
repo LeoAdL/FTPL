4a4cf420d40b757156d239a72ffe4be811ebd942
LeoAdL <laparisidelannoy@uchicago.edu>
2021-04-15 18:59:28 -0500

    seems fine

--- HW1.jl
+++ HW1.jl
@@ -1,4 +1,3 @@
-using DifferentialEquations
 using BoundaryValueDiffEq
 using LinearAlgebra, Statistics, Plots
 using Parameters,LaTeXStrings
@@ -11,14 +10,18 @@ using Printf
 #Schaab + KMV 2018
 
 
-function define_env(;ρ̄      =  .022,
+function define_env(;ρ̄      =  .0022,
                      θᵨ     =   .22,
                      θᵢ      =   1.0,
                      θ      =   100.0,
                      ϵ      =   10,
                      ψ      =   2.0,
                      ϕ      =   1.25,
-                     γ      =   2.0)
+                     γ      =   2.0,
+                     T      =   10.0,
+                     dt     =   .1,
+                     σᵨ     =   0.003)
+    init_ρ =   ρ̄+sqrt(σᵨ^2/(1-θᵨ^2))
     σ   =   1/γ
     params  =   @with_kw (ρ̄      =  ρ̄,
                      θᵨ     =   θᵨ,
@@ -29,8 +32,9 @@ function define_env(;ρ̄      =  .022,
                      ϕ      =   ϕ,
                      σ      =   σ,
                      γ      =   γ,
-                     T      =   10.0,
-                     dt     =   .01)
+                     T      =   T,
+                     dt     =   dt,
+                     init_ρ=init_ρ)
     return params
 end
 
@@ -39,22 +43,52 @@ params  =   define_env()
 function NK_Rote!(du,u,p,t)
     @unpack σ,ϵ,θ,σ,ϕ,ψ,ρ̄,θᵨ,θᵢ =   params
     x    =   u[1]
-    dx   =   du[1]
 
     π   =   u[2]
-    dπ  =   du[2]
 
     i   =   u[3]
-    di  =   du[3]
 
     ρ   =   u[4]
-    dρ  =   du[4]
 
-    dx  =   σ*(i-π-ρ)*x
+    du[1]  =   σ*(i-π-ρ)*x
     
-    dπ  =   (ϵ-1)/θ*(x^(1\σ+ψ)-1)-π*((1-σ)*(i-π)+σ*ρ)
+    du[2]  =   (ϵ-1.0)/θ*(x^(1\σ+ψ)-1.0)-π*((1.0-σ)*(i-π)+σ*ρ)
 
-    di  =   -θᵢ*(i-ϕ*π)
+    du[3]  =   -θᵢ*(i-ϕ*π)
 
-    dρ  =   -θᵨ*(ρ-ρ̄)
+    du[4]  =   -θᵨ*(ρ-ρ̄)
+    
 end
+
+function SS()
+    @unpack σ,ϵ,θ,σ,ϕ,ψ,ρ̄,θᵨ,θᵢ =   params
+
+    ρ_ss    =   ρ̄
+    π_ss    =   ρ̄/(ϕ-1.0)
+    i_ss    =   ρ̄*ϕ/(ϕ-1.0)
+    x_ss    =   (1.0+θ/(ϵ-1.0)*π_ss*(σ*ρ̄+(1-σ)*(i_ss-π_ss)))^(1.0/(1.0/σ+ψ))
+
+    return (ρ_ss=ρ_ss,π_ss=π_ss,i_ss=i_ss,x_ss=x_ss)
+end
+
+function bc1!(residual,u,p,t)
+    @unpack ρ_ss,π_ss,i_ss,x_ss= SS()
+    @unpack init_ρ  =   params
+    residual[1] =   u[end][1]- x_ss
+    residual[2] =   u[end][2]- π_ss
+    residual[3] =   u[1][3]- i_ss
+    residual[4] =   u[1][4]- init_ρ
+end
+
+@unpack ρ_ss,π_ss,i_ss,x_ss= SS()
+@unpack T,dt,init_ρ = params
+
+tspan   =   (0.0,T)
+
+
+bvp1 = BVProblem(NK_Rote!, bc1!, [x_ss,π_ss,i_ss,init_ρ], tspan)
+
+sol1 = solve(bvp1, GeneralMIRK4(), dt=dt)
+
+dev =   sol1[1:end].-[x_ss,π_ss,i_ss,init_ρ]
+plot(sol1.t,dev')
