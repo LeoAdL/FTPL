f08aa2b41e8e205677eea360b7a040a4065956ab
LeoAdL <laparisidelannoy@uchicago.edu>
2021-04-21 18:52:23 -0500

    Ended?

--- Function_quadratic.jl
+++ Function_quadratic.jl
@@ -58,9 +58,11 @@ function solve_system(;params)
         @unpack α=params
         return((w/(1-α))^(1-α)*(q*ν_k/α)^(α))
     end
+
     function NK_natural()
         @unpack σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ =   params
         function obj(u)
+            @unpack σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ =   params
             F   =   zeros(6)
             π=u[1]
             C=u[2]
@@ -70,7 +72,7 @@ function solve_system(;params)
             i=u[6]
 
             F[1]=δ+(i-π)+(ι(q)+κ*(ι(q))/2)/q-ι(q)
-            F[1]=F[1]-ν_k(q,w(ℓ,C),ℓ(C,k,ι),k)
+            F[1]=F[1]-ν_k(q,w(ℓ(C,k,ι),C),ℓ(C,k,ι),k)
 
             F[2]=i-π-ρ
             
@@ -81,66 +83,103 @@ function solve_system(;params)
             F[5]=ρ̄-ρ
 
             F[6]=i-ϕ*π
+            return(F)
         end
+        u = nlsolve(obj, zeros(6))
+
+        π=u[1]
+        C=u[2]
+        q=u[3]
+        k=u[4]
+        ρ=u[5]
+        i=u[6]
+
+        ℓ=ℓ(C,k,ι)
+        w=w(ℓ,C)
+        ν=ν_k(q,w,ℓ,k)
+        χₙ=χ(w,q,ν_k)
+
+        return(π=π,
+                C=C,
+                q=q,
+                k=k,
+                ρ=ρ,
+                i=i,
+                χₙ=χₙ)
     end
 
     function NK!(du,u,p,t)
-        @unpack σ,ϵ,θ,σ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ =   params
-        x    =   u[1]
+        @unpack σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ =   params
+        χₙ=NK_natural().χₙ
+            q=u[1]
+            C=u[2]
+            k=u[3]
+            π=u[4]
+            ρ=u[5]
+            i=u[6]
 
-        π   =   u[2]
+            du[1]=δ+(i-π)+(ι(q)+κ*(ι(q))/2)/q-ι(q)
+            du[1]=du[1]-ν_k(q,w(ℓ(C,k,ι),C),ℓ(C,k,ι),k)
+            du[1]=du[1]*q
 
-        i   =   u[3]
+            du[2]=σ*C*(i-π-ρ)
+            
+            du[3]=(ι(q)-δ-(ι(q)+κ*(ι(q))/2)/q)*k
 
-        ρ   =   u[4]
+            du[4]=+((1-σ)*(i-π)+σ*ρ)*π
 
-        du[1]  =   σ*(i-π-ρ)*x
-        
-        du[2]  =   -(ϵ-1.0)/θ*(x^(1.0\σ+ψ)-1.0)+π*((1.0-σ)*(i-π)+σ*ρ)
+            du[4]=du[4]-(ϵ-1)/(θ)*(χ(w(ℓ(C,k,ι),C),q,ν_k(q,w(ℓ(C,k,ι),C),ℓ(C,k,ι),k))/χₙ-1)
 
-        du[3]  =   -θᵢ*(i-ϕ*π)
+            du[5]=θᵨ*(ρ̄-ρ)
 
-        du[4]  =   -θᵨ*(ρ-ρ̄)
+            du[6]=-θᵢ*(i-ϕ*π)
         
     end
 
     function SS()
-        @unpack σ,ϵ,θ,σ,ϕ,ψ,ρ̄,θᵨ,θᵢ =   params
-
-        ρ_ss    =   ρ̄
-        π_ss    =   ρ̄/(ϕ-1.0)
-        i_ss    =   ρ̄*ϕ/(ϕ-1.0)
-        x_ss    =   (1.0+θ/(ϵ-1.0)*π_ss*(σ*ρ̄+(1-σ)*(i_ss-π_ss)))^(1.0/(1.0/σ+ψ))
-
-        return (ρ_ss=ρ_ss,π_ss=π_ss,i_ss=i_ss,x_ss=x_ss)
+    u = solve(NK,SSRootfind())
+            q_ss=u[1]
+            C_ss=u[2]
+            k_ss=u[3]
+            π_ss=u[4]
+            ρ_ss=u[5]
+            i_ss=u[6]
+       return(π_ss=π_ss,
+                C_ss=C_ss,
+                q_ss=q_ss,
+                k_ss=k_ss,
+                ρ_ss=ρ_ss,
+                i_ss=i_ss)    
     end
 
     function bc1!(residual,u,p,t)
-        @unpack ρ_ss,π_ss,i_ss,x_ss= SS()
+        @unpack q_ss,C_ss,k_ss,π_ss,ρ_ss,i_ss= SS()
         @unpack init_ρ  =   params
-        residual[1] =   u[end][1]- x_ss
-        residual[2] =   u[end][2]- π_ss
-        residual[3] =   u[1][3]- i_ss
-        residual[4] =   u[1][4]- init_ρ
+        residual[1] =   u[end][1]- q_ss
+        residual[2] =   u[end][2]- C_ss
+        residual[3] =   u[end][3]- k_ss
+        residual[4] =   u[end][4]- π_ss
+        residual[5] =   u[1][5]- init_ρ
+        residual[6] =   u[1][6]- i_ss
     end
 
-    @unpack ρ_ss,π_ss,i_ss,x_ss= SS()
+    @unpack q_ss,C_ss,k_ss,π_ss,ρ_ss,i_ss= SS()
     @unpack T,dt,init_ρ = params
 
-    SS_vec = [x_ss,π_ss,i_ss,ρ_ss]
+    SS_vec = [q_ss,C_ss,k_ss,π_ss,ρ_ss,i_ss]
 
-    init    =   [x_ss,π_ss,i_ss,init_ρ]
+    init    =   [q_ss,C_ss,k_ss,π_ss,init_ρ,i_ss]
     tspan   =   (0.0,T)
 
 
-    bvp1 = TwoPointBVProblem(NK_Rote!, bc1!, init, tspan)
+    bvp1 = TwoPointBVProblem(NK!, bc1!, init, tspan)
 
     sol1 = solve(bvp1, MIRK4(), dt=dt)
     return (sol=sol1,SS=SS_vec,initial=init,t=sol1.t)
 end
 
-function plot_IRF(;pos =[1,2,3,4],solution)
-    val =["x","\\pi","i","\\rho"]
+function plot_IRF(;pos =[1,2,3,4,5,6],solution)
+    val =["q","Y","k","\\pirho","i",]
     val =val[pos]
     lab=[latexstring("\$\\widehat{{$(u)}}_{t}\$") for u in val]
     lab=reshape(lab,(1,length(val)))
@@ -163,10 +202,10 @@ end
 
 function compute_dev(;θ,T)
         solution=solve_system(;params=define_env(θ=θ))
-        SS  =   solution.SS[1]
+        SS  =   solution.SS[2]
         dev =   ((solution.sol[1,1:end].-SS)./SS)*100
         cum = sum(dev[1:floor(Int,T)])
-        return (impact=dev[1],cum=cum)
+        return (impact=dev[2],cum=cum)
 end
 
 function plot_θ_impact(;theta_range=range(10^(-3),500,length=10))
