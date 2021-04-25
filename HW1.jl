608bf69fbadbc9ad4d9f3ca8c5097f976d2a0447
LeoAdL <laparisidelannoy@uchicago.edu>
2021-04-25 17:38:26 -0500

    Last changes

--- HW1.jl
+++ HW1.jl
@@ -14,14 +14,14 @@ include("Functions.jl")
 include("Function_quadratic.jl")
 @unpack T=define_env(κ=10)
 
-solution_NK=solve_system(;params=define_env())
+solutionNK=solve_system(;params=define_env())
 
-solution=solve_system_quad(;params=define_env(κ=10))
-
-plot_IRF(solution=solution_NK)
+plot_IRF(solution=solutionNK,var=["x","\\pi","i"])
 
 
-plot_IRF_quad(;var =["k","\\iota","Y"],solution=solution,T_end=200)
+plot_IRF_quad(;var =["k","\\iota","Y"],
+                solution=solve_system_quad(;params=define_env(κ=10)),
+                T_end=200)
 
 @time plot_θ_cum_quad(;var="Y",theta_range=range(.1,500,length=2),κ_range=[10,10000],T_range=[0])
 
