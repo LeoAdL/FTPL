552283e31f7291706b6124dc8181cc0c8c84dbf1
LeoAdL <laparisidelannoy@uchicago.edu>
2021-04-27 10:16:51 -0500

    FTPL_seems good

--- Function_quadratic.jl
+++ Function_quadratic.jl
@@ -1,7 +1,7 @@
 function solve_system_quad(;params)
 
 function static_funct(p)
-    @unpack  σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ,A,χₙ,α,γ =   p
+    @unpack σ, ϵ, θ, ϕ, ψ, ρ̄, θᵨ, θᵢ, κ, δ, A, χₙ, α, γ = p
     function ι(q::Float64)
         return((q-1.0)/κ)
     end
@@ -32,72 +32,72 @@ end
 
 
     function NK!(du,u,p,t)
-         σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ,A,χₙ =   p
-         @unpack ι,ℓ,w,ν_k,χ,Y=static_funct(p)
-            q=u[1]
-            C=u[2]
-            k=u[3]
-            π=u[4]
-            ρ=u[5]
-            i=u[6]
+         σ      ,ϵ, θ, ϕ, ψ,   ρ̄, θᵨ, θᵢ, κ, δ, A, χₙ = p
+         @unpack ι, ℓ, w, ν_k, χ,  Y  = static_funct(p)
+                 q = u[1]
+                 C = u[2]
+                 k = u[3]
+                 π = u[4]
+                 ρ = u[5]
+                 i = u[6]
 
-            du[1]=q*(i-π+(ι(q)+κ*(ι(q))^(2.0)/2.0)/q-ι(q)-ν_k(C,k,q))
+            du[1] = q*(i-π+(ι(q)+κ*(ι(q))^(2.0)/2.0)/q-ι(q)-ν_k(C,k,q))
             
-            du[2]=σ*C*(i-π-ρ)
+            du[2] = σ*C*(i-π-ρ)
             
-            du[3]=ι(q)*k
+            du[3] = ι(q)*k
 
-            du[4]=π*((1.0-σ)*(i-π)+σ*ρ)-((ϵ-1.0)/θ)*(χ(C,k,q)/χₙ-1.0)
+            du[4] = π*((1.0-σ)*(i-π)+σ*ρ)-((ϵ-1.0)/θ)*(χ(C,k,q)/χₙ-1.0)
             
-            du[5]=-θᵨ*(ρ-ρ̄)
+            du[5] = -θᵨ*(ρ-ρ̄)
 
-            du[6]=-θᵢ*(i-ϕ*π)
+            du[6] = -θᵢ*(i-ϕ*π)
         
     end
 
     function SS(p)
-        @unpack α,γ,σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,κ,δ,A =   p
+        @unpack α, γ, σ, ϵ, θ, ϕ, ψ, ρ̄, θᵨ, θᵢ, κ, δ, A = p
 
-            k_c=(α/(ρ̄))*((ϵ-1.0)/ϵ)*(1.0+θ/(ϵ-1.0)*ρ̄^(2)/(ϕ-1.0))
+            k_c = (α/(ρ̄))*((ϵ-1.0)/ϵ)*(1.0+θ/(ϵ-1.0)*ρ̄^(2)/(ϕ-1.0))
     
-            k_l=(A*k_c)^(1.0/(1.0-α))
+            k_l = (A*k_c)^(1.0/(1.0-α))
 
-            q_ss=1.0
-            k_ss=(ρ̄*((1-α)/α)*(k_l)^(1+ψ)*(k_c)^(γ))^(1/(ψ+γ))
-            C_ss=k_ss/k_c
-            π_ss=ρ̄/(ϕ-1.0)
-            ρ_ss=ρ̄
-            i_ss=ϕ*π_ss
+            q_ss = 1.0
+            k_ss = (ρ̄*((1-α)/α)*(k_l)^(1+ψ)*(k_c)^(γ))^(1/(ψ+γ))
+            C_ss = k_ss/k_c
+            π_ss = ρ̄/(ϕ-1.0)
+            ρ_ss = ρ̄
+            i_ss = ϕ*π_ss
        return(π_ss=π_ss,
-                C_ss=C_ss,
-                q_ss=q_ss,
-                k_ss=k_ss,
-                ρ_ss=ρ_ss,
-                i_ss=i_ss,
-                ℓ_ss=k_ss/k_l,
-                ι_ss=0.0)    
+                C_ss = C_ss,
+                q_ss = q_ss,
+                k_ss = k_ss,
+                ρ_ss = ρ_ss,
+                i_ss = i_ss,
+                ℓ_ss = k_ss/k_l,
+                ι_ss = 0.0)
     end
 
     function u_0(p)
-        @unpack q_ss,C_ss,k_ss,π_ss,i_ss,ℓ_ss,ι_ss = SS(p)
-        @unpack init_ρ  =   p
+        @unpack q_ss,   C_ss, k_ss, π_ss, i_ss, ℓ_ss, ι_ss = SS(p)
+        @unpack init_ρ = p
     return ([q_ss,C_ss,k_ss,π_ss,p.init_ρ,i_ss])
     end
 
     p  =    (σ=params.σ,ϵ=params.ϵ,θ=params.θ,
-            ϕ=params.ϕ,ψ=params.ψ,ρ̄=params.ρ̄,θᵨ=params.θᵨ,
-            θᵢ=params.θᵢ,κ=params.κ,δ=params.δ,A=params.A,
-            χₙ=params.χₙ,γ=params.γ,α=params.α,init_ρ=params.init_ρ)
+            ϕ  = params.ϕ,  ψ = params.ψ, ρ̄ = params.ρ̄, θᵨ     = params.θᵨ,
+            θᵢ = params.θᵢ, κ = params.κ, δ  = params.δ,  A      = params.A,
+            χₙ = params.χₙ, γ = params.γ, α  = params.α,  init_ρ = params.init_ρ)
 
     function bc1!(residual,u,p,t)
-            @unpack q_ss,C_ss,k_ss,π_ss,ρ_ss,i_ss= SS(p)
-            @unpack init_ρ  =   p
-            residual[1] =   u[end][1]- q_ss
-            residual[2] =   u[end][2]- C_ss
-            residual[3] =   u[1][3]- k_ss
-            residual[4] =   u[end][4]- π_ss
-            residual[5] =   u[1][5]- init_ρ
-            residual[6] =   u[1][6]- i_ss
+            @unpack  q_ss,   C_ss, k_ss, π_ss, ρ_ss, i_ss = SS(p)
+            @unpack  init_ρ = p
+            residual[1]     = u[end][1]- q_ss
+            residual[2]     = u[end][2]- C_ss
+            residual[3]     = u[1][3]- k_ss
+            residual[4]     = u[end][4]- π_ss
+            residual[5]     = u[1][5]- init_ρ
+            residual[6]     = u[1][6]- i_ss
     end
 
     bvp1 = TwoPointBVProblem(NK!, bc1!, u_0(p), (0.0,params.T),(p))
@@ -105,78 +105,78 @@ end
     u = solve(bvp1, MIRK4(), dt=params.dt)
     
     function result(u,p)
-    @unpack δ =p
-    @unpack q_ss,C_ss,k_ss,π_ss,i_ss,ℓ_ss,ι_ss,ρ_ss = SS(p)
-    q=@view u[1,:][:]
-    C=@view u[2,:][:]
-    k=@view u[3,:][:]
-    π=@view u[4,:][:]
-    i=@view u[6,:][:] 
+    @unpack δ    = p
+    @unpack q_ss, C_ss, k_ss, π_ss, i_ss, ℓ_ss, ι_ss, ρ_ss = SS(p)
+            q    = @view u[1,:][:]
+            C    = @view u[2,:][:]
+            k    = @view u[3,:][:]
+            π    = @view u[4,:][:]
+            i    = @view u[6,:][:]
 
-    sol1  =   similar(zeros(size(u)[1]+3,size(u)[2]))
-    sol1[1:size(u)[1]-1,:]  =@view u[2:end,:]
+         sol1              = similar(zeros(size(u)[1]+3,size(u)[2]))
+    sol1[1:size(u)[1]-1,:] = @view u[2:end,:]
 
-    @unpack ι,ℓ,w,ν_k,χ,Y=static_funct(p)
+    @unpack ι, ℓ, w, ν_k, χ, Y = static_funct(p)
 
-    sol1[6,:] =   ι.(q) .+ δ
-    sol1[7,:] =   ℓ.(C,k,q)
-    sol1[8,:] =   Y.(C,k,q)
-    sol1[9,:] =   i.-π
+    sol1[6,:] = ι.(q) .+ δ
+    sol1[7,:] = ℓ.(C,k,q)
+    sol1[8,:] = Y.(C,k,q)
+    sol1[9,:] = i.-π
     
-    SS_vec=similar(sol1[:,1])
-    SS_vec[1]=C_ss
-    SS_vec[2]=k_ss
-    SS_vec[3]=π_ss
-    SS_vec[4]=ρ_ss
-    SS_vec[5]=i_ss
-    SS_vec[6]=ι_ss+ δ
-    SS_vec[7]=ℓ_ss
-    SS_vec[8]=Y.(C_ss,k_ss,q_ss)
-    SS_vec[9]=i_ss-π_ss
+           SS_vec = similar(sol1[:,1])
+    SS_vec[1]     = C_ss
+    SS_vec[2]     = k_ss
+    SS_vec[3]     = π_ss
+    SS_vec[4]     = ρ_ss
+    SS_vec[5]     = i_ss
+    SS_vec[6]     = ι_ss+ δ
+    SS_vec[7]     = ℓ_ss
+    SS_vec[8]     = Y.(C_ss,k_ss,q_ss)
+    SS_vec[9]     = i_ss-π_ss
     return(SS_vec=SS_vec,sol1=sol1)
     end
 
 
-    @unpack sol1,SS_vec = result(u,p)
+    @unpack sol1, SS_vec = result(u,p)
     return (sol=sol1,SS=SS_vec,t=u.t)
 end
 
-@unpack T,ϕ,dt   = pp
+@unpack T, ϕ, dt = pp
 
 function plot_IRF_quad(;var =["C","k","\\pi","\\rho","i","\\iota","\\ell","Y","r"],
                         solution,T_end=T)
-    N_end=T_end/dt+1
-    val =["C","k","\\pi","\\rho","i","\\iota","\\ell","Y","r"]
-    pos=(zeros(length(var)))
-    for k in 1:length(var)
-        pos[k]=findfirst(isequal(var[k]),val)
+    N_end = T_end/dt+1
+    val   = ["C","k","\\pi","\\rho","i","\\iota","\\ell","Y","r"]
+    pos   = (zeros(length(var)))
+    for k in 1: length(var)
+        pos[k] = findfirst(isequal(var[k]),val)
     end
-    pos=round.(Int, pos)
-    val =val[pos]
-    lab=[latexstring("\$\\widehat{{$(u)}}_{t}\$") for u in val]
-    lab=reshape(lab,(1,length(val)))
+    pos = round.(Int, pos)
+    val = val[pos]
+    lab = [latexstring("\$\\widehat{{$(u)}}_{t}\$") for u in val]
+    lab = reshape(lab,(1,length(val)))
 
-    SS  =   solution.SS
-    dev =   ((solution.sol.-SS)./SS)*100
+    SS  = solution.SS
+    dev = ((solution.sol.-SS)./SS)*100
 
      
     pp = [dev[k,1:round(Int,N_end)] for k in pos]
 
     p=plot(solution.t[1:round(Int,N_end)],pp,
-        label=lab,
-        xlabel=L"t",
-        legendfontsize=8,
-        ylabel=L"\%",
-        legend=:outertopright)
+        label          = lab,
+        xlabel         = L"t",
+        legendfontsize = 8,
+        ylabel         = L"\%",
+        legend         = :outertopright)
     display(p)
     return(p)
 end
 
 
 function compute_dev_quad(;solution,n,T)
-        SS  =   solution.SS[n]
-        dev =   (((@view solution.sol[n,:]).-SS)./SS)*100
-        N=T/dt+1
+        SS  = solution.SS[n]
+        dev = (((@view solution.sol[n,:]).-SS)./SS)*100
+        N   = T/dt+1
         cum = sum(@view dev[1:floor(Int,N)])
         return (cum)
 end
@@ -184,39 +184,39 @@ end
 
 function plot_θ_cum_quad(;var="Y",θ_range=range(.1,500,length=10),ϕ=ϕ,
                 T_range=[T],κ_range=[3,30,300])
-    val =["C","k","\\pi","\\rho","i","\\iota","\\ell","Y","r"]
-    n=findfirst(isequal(var), val)
-    N   = length(T_range)*length(κ_range)    
-    lab=[latexstring("\$T={$(T)},\\kappa={$(κ)}\$") for (T,κ) in Iterators.product(T_range, κ_range)][:]
-    lab=reshape(lab,1,N)
+    val = ["C","k","\\pi","\\rho","i","\\iota","\\ell","Y","r"]
+    n   = findfirst(isequal(var), val)
+    N   = length(T_range)*length(κ_range)
+    lab = [latexstring("\$T={$(T)},\\kappa={$(κ)}\$") for (T,κ) in Iterators.product(T_range, κ_range)][:]
+    lab = reshape(lab,1,N)
 
     y = similar(zeros(length(θ_range),N))
-    j=0
+    j = 0
     for θ in θ_range
-        j=j+1
-        k=0
+        j = j+1
+        k = 0
         for κ in κ_range
-        solution=solve_system_quad(;params=define_env(θ=θ,κ=κ,T=T,N_t=T/dt,ϕ=ϕ))
+        solution = solve_system_quad(;params=define_env(θ=θ,κ=κ,T=T,N_t=T/dt,ϕ=ϕ))
             for T in T_range
-            k = k+1
+              k    = k+1
             y[j,k] = compute_dev_quad(;solution=solution,n=n,T=T)
             end
         end
     end
-    lines=[:dash for k in 1:N]
-    for k in 1:floor(Int,N/2)
-        lines[2*k] = :solid
+    lines = [:dash for k in 1:N]
+    for   k in 1: floor(Int,N/2)
+    lines[2*k] =:solid
     end
     lines = reshape(lines,1,N)
-    p=plot(θ_range,
+    p     = plot(θ_range,
             y, 
-            label=lab,
-            xlabel=L"\theta", 
-            ylabel=latexstring("\$\\sum_{t=0}{T}\\widehat{{$(val[n])}}_{t}\\left(\\%,\\phi=$(ϕ)\\right)\$"),
-            legendfontsize=7,
-            palette = palette([:blue,:red],N),
-            linestyle = lines,
-            legend=:outertopright)
+            label          = lab,
+            xlabel         = L"\theta",
+            ylabel         = latexstring("\$\\sum_{t=0}{T}\\widehat{{$(val[n])}}_{t}\\left(\\%,\\phi=$(ϕ)\\right)\$"),
+            legendfontsize = 7,
+            palette        = palette([:blue,:red],N),
+            linestyle      = lines,
+            legend         = :outertopright)
     savefig(p,"theta_cum_$(val[n])_$(T_range[1])_quad.svg")
     display(p)
 end
--- NK_FTPL_no_K.jl
+++ NK_FTPL_no_K.jl
@@ -1,166 +1,176 @@
 function solve_system(;params)
 
     function SS(p)
-        @unpack σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,A,S,γ,ind_Taylor,i_target,ϕ_FTPL,s₀ =   p
+        @unpack σ, ϵ, θ, ϕ, ψ, ρ̄, θᵨ, θᵢ, A, S, γ, ind_Taylor, i_target, ϕ_FTPL, s₀ = p
 
-        Yₙ  =   A^(ψ/(ψ+γ))*((ϵ-1)/ϵ)^(1/(ψ+γ))
+        Yₙ = A^(ψ/(ψ+γ))*((ϵ-1)/ϵ)^(1/(ψ+γ))
 
-        ρ_ss    =   ρ̄
-        i_ss    =   ρ̄*ϕ_FTPL/(ϕ_FTPL-1.0)*ind_Taylor +(1-ind_Taylor)*i_target
-        π_ss    =   i_ss - ρ_ss
-        x_ss    =   (1.0+θ/(ϵ-1.0)*π_ss*(σ*ρ̄+(1-σ)*(i_ss-π_ss)))^(1.0/(1.0/σ+ψ))
-        v_ss    =   s₀/(i_ss-π_ss-S)
-        s_ss    =   s₀ + S*v_ss
+        ρ_ss = ρ̄
+        i_ss = ρ̄*ϕ_FTPL/(ϕ_FTPL-1.0)*ind_Taylor +(1-ind_Taylor)*i_target
+        π_ss = i_ss - ρ_ss
+        x_ss = (1.0+θ/(ϵ-1.0)*π_ss*(σ*ρ̄+(1-σ)*(i_ss-π_ss)))^(1.0/(1.0/σ+ψ))
+        v_ss = s₀/(i_ss-π_ss-S)
+        s_ss = s₀ + S*v_ss
         return (ρ_ss=ρ_ss,π_ss=π_ss,i_ss=i_ss,
                 x_ss=x_ss,v_ss=v_ss,Yₙ=Yₙ,s_ss=s_ss)
     end
 
     function NK_FTPL!(du,u,p,t)
-        @unpack σ,ϵ,θ,ϕ,ψ,ρ̄,θᵨ,θᵢ,S,ϕ_FTPL,ind_Taylor,s₀ =   p
-        @unpack ρ_ss,v_ss,Yₙ,x_ss,i_ss,π_ss = SS(p)
-        x    =   u[1]
+        @unpack σ,    ϵ,    θ,  ϕ,    ψ,    ρ̄,   θᵨ, θᵢ, S, ϕ_FTPL, ind_Taylor, s₀ = p
+        @unpack ρ_ss, v_ss, Yₙ, x_ss, i_ss, π_ss = SS(p)
+                x    = u[1]
 
-        π   =   u[2]
+        π = u[2]
 
-        i   =   u[3]
+        i = u[3]
 
-        ρ   =   u[4]
+        ρ = u[4]
 
-        v   =   u[5]
+        v = u[5]
 
-        vˡ  =   u[6]
+        vˡ = u[6]
 
 
-        du[1]  =   σ*(i-π-ρ)*x
+        du[1] = σ*(i-π-ρ)*x
         
-        du[2]  =   -(ϵ-1.0)/θ*(x^(1.0/σ+ψ)-1.0)+π*((1.0-σ)*(i-π)+σ*ρ)
+        du[2] = -(ϵ-1.0)/θ*(x^(1.0/σ+ψ)-1.0)+π*((1.0-σ)*(i-π)+σ*ρ)
 
-        du[3]  = -θᵢ*(i-ϕ_FTPL*π)*ind_Taylor 
+        du[3] = -θᵢ*(i-ϕ_FTPL*π)*ind_Taylor
 
-        du[4]  =   -θᵨ*(ρ-ρ̄)
+        du[4] = -θᵨ*(ρ-ρ̄)
 
-        du[5]   =   v*(i-π) -(s₀+S*vˡ)
+        du[5] = v*(i-π) -(s₀+S*vˡ)
 
-        du[6]   =   vˡ *(i-π) -(s₀+S*vˡ)
+        du[6] = vˡ *(i-π) -(s₀+S*vˡ)
 
     end
 
     
     function u_0(p)
-    @unpack π_ss,i_ss,x_ss,v_ss = SS(p)
-    @unpack init_ρ  =   p
+    @unpack π_ss,   i_ss, x_ss, v_ss = SS(p)
+    @unpack init_ρ = p
     return ([x_ss,π_ss,i_ss,init_ρ,v_ss,v_ss])
     end
 
 
-    p  =(σ=params.σ,ϵ=params.ϵ,θ=params.θ,
-        ϕ=params.ϕ,ψ=params.ψ,ρ̄=params.ρ̄,θᵨ=params.θᵨ,θᵢ=params.θᵢ,
-        γ=params.γ,init_ρ=params.init_ρ,
-        S=params.S,A=params.A,i_target=params.i_target,
-        ind_Taylor=params.ind_Taylor,ϕ_FTPL=params.ϕ_FTPL,s₀=params.s₀)
+    p  =(σ=params.σ,
+        ϵ          = params.ϵ,
+        θ          = params.θ,
+        ϕ          = params.ϕ,
+        ψ          = params.ψ,
+        ρ̄         = params.ρ̄,
+        θᵨ         = params.θᵨ,
+        θᵢ         = params.θᵢ,
+        γ          = params.γ,
+        init_ρ     = params.init_ρ,
+        S          = params.S,
+        A          = params.A,
+        i_target   = params.i_target,
+        ind_Taylor = params.ind_Taylor,
+        ϕ_FTPL     = params.ϕ_FTPL,
+        s₀         = params.s₀)
 
 
     function bc1!(residual,u,p,t)
-        @unpack ρ_ss,π_ss,i_ss,x_ss,v_ss= SS(p)
-        @unpack init_ρ  =   p
-        residual[1] =   u[end][1]- x_ss
-        residual[2] =   u[end][2]- π_ss
-        residual[3] =   u[1][3]- i_ss
-        residual[4] =   u[1][4]- init_ρ
-        residual[5] =   u[end][5]- v_ss
-        residual[6] =   u[1][6]- v_ss
+        @unpack  ρ_ss,   π_ss, i_ss, x_ss, v_ss = SS(p)
+        @unpack  init_ρ = p
+        residual[1]     = u[end][1]- x_ss
+        residual[2]     = u[end][2]- π_ss
+        residual[3]     = u[1][3]- i_ss
+        residual[4]     = u[1][4]- init_ρ
+        residual[5]     = u[end][5]- v_ss
+        residual[6]     = u[1][6]- v_ss
     end
 
-    
     function SS_vec(p)
-    @unpack ρ_ss,π_ss,i_ss,x_ss,v_ss,s_ss= SS(p)
+    @unpack ρ_ss, π_ss, i_ss, x_ss, v_ss, s_ss = SS(p)
     return([x_ss,π_ss,i_ss,ρ_ss,v_ss,s_ss])
     end
 
-    @unpack T,dt,init_ρ = params
-    bvp1 = TwoPointBVProblem(NK_FTPL!, bc1!,u_0(p), (0.0,T),(p))
-    u = solve(bvp1, MIRK4(), dt=dt)
+    @unpack T,    dt, init_ρ = params
+            bvp1 = TwoPointBVProblem(NK_FTPL!, bc1!,u_0(p), (0.0,T),(p))
+            u    = solve(bvp1, MIRK4(), dt=dt)
     
     function sol(u,p)
-        @unpack s₀,S=p
-        vˡ  =   @view u[6,:][:]
+        @unpack s₀, S = p
+                vˡ = @view u[6,:][:]
 
 
-            sol  =   similar(zeros(size(u)[1],size(u)[2]))
-            sol[1:size(u)[1],:]  =@view u[:,:]
-            sol[size(u)[1],:] = s₀.+S*vˡ
+                sol             = similar(zeros(size(u)[1],size(u)[2]))
+            sol[1:size(u)[1],:] = @view u[:,:]
+            sol[size(u)[1],:]   = s₀.+S*vˡ
         return(sol)
     end
     return (sol=sol(u,p),SS=SS_vec(p),t=u.t)
 end
 
-@unpack T,ϕ,dt   = pp
+@unpack T, ϕ, dt = pp
 
 function plot_IRF_FTPL(;var =["x","\\pi","i","\\rho","v","s"],
                         solution,T_end=T)
-    N_end=T_end/dt+1
-    val =["x","\\pi","i","\\rho","v","s"]
-    pos=(zeros(length(var)))
-    for k in 1:length(var)
-        pos[k]=findfirst(isequal(var[k]),val)
+    N_end = T_end/dt+1
+    val   = ["x","\\pi","i","\\rho","v","s"]
+    pos   = (zeros(length(var)))
+    for k in 1: length(var)
+        pos[k] = findfirst(isequal(var[k]),val)
     end
-    pos=round.(Int, pos)
-    val =val[pos]
-    lab=[latexstring("\$\\widehat{{$(u)}}_{t}\$") for u in val]
-    lab=reshape(lab,(1,length(val)))
+    pos = round.(Int, pos)
+    val = val[pos]
+    lab = [latexstring("\$\\widehat{{$(u)}}_{t}\$") for u in val]
+    lab = reshape(lab,(1,length(val)))
 
-    SS  =   solution.SS
-    dev =   ((solution.sol.-SS)./SS)*100
+    SS  = solution.SS
+    dev = ((solution.sol.-SS)./SS)*100
 
      
     pp = [dev[k,1:round(Int,N_end)] for k in pos]
 
     p=plot(solution.t[1:round(Int,N_end)],pp,
-        label=lab,
-        xlabel=L"t",
-        legendfontsize=8,
-        ylabel=L"\%",
-        legend=:outertopright)
+        label          = lab,
+        xlabel         = L"t",
+        legendfontsize = 8,
+        ylabel         = L"\%",
+        legend         = :outertopright)
     display(p)
     return(p)
 end
 
 
 function compute_dev_FTPL(;solution,n,T)
-        SS  =   solution.SS[n]
-        dev =   (((@view solution.sol[n,:]).-SS)./SS)*100
-        N=T/dt+1
+        SS  = solution.SS[n]
+        dev = (((@view solution.sol[n,:]).-SS)./SS)*100
+        N   = T/dt+1
         cum = sum(@view dev[1:floor(Int,N)])
         return (cum)
 end
 
 function plot_θ_cum(;var="x",θ_range=range(1,500,length=10),ϕ=ϕ,
                 T_range=[0,T],ind_Taylor=pp.ind_Taylor,ϕ_FTPL=pp.ϕ_FTPL)
-    val =["x","\\pi","i","\\rho","v","s"]
-    n=findfirst(isequal(var), val)
-    N   = length(T_range)  
-    lab=[latexstring("\$T={$(T)}\$") for T in T_range]
-    lab=reshape(lab,1,N)
+    val = ["x","\\pi","i","\\rho","v","s"]
+    n   = findfirst(isequal(var), val)
+    N   = length(T_range)
+    lab = [latexstring("\$T={$(T)}\$") for T in T_range]
+    lab = reshape(lab,1,N)
 
     y = similar(zeros(length(θ_range),N))
-    j=0
+    j = 0
     for θ in θ_range
-        j=j+1
-        k=0
-        solution=solve_system(;params=define_env(θ=θ,T=T,N_t=T/dt,ϕ_FTPL=ϕ_FTPL,ind_Taylor=ind_Taylor))
+        j        = j+1
+        k        = 0
+        solution = solve_system(;params=define_env(θ=θ,T=T,N_t=T/dt,ϕ_FTPL=ϕ_FTPL,ind_Taylor=ind_Taylor))
             for T in T_range
-            k = k+1
+              k    = k+1
             y[j,k] = compute_dev_FTPL(;solution=solution,n=n,T=T)
         end
     end
     p=plot(θ_range,
             y, 
-            label=lab,
-            xlabel=L"\theta", 
-            ylabel=latexstring("\$\\sum_{t=0}{T}\\widehat{{$(val[n])}}_{t}\\left(\\%,\\phi=$(ϕ_FTPL*ind_Taylor)\\right)\$"),
-            legendfontsize=7,
-            palette = palette([:blue,:red],N),
-            legend=:outertopright)
+            label          = lab,
+            xlabel         = L"\theta",
+            ylabel         = latexstring("\$\\sum_{t=0}{T}\\widehat{{$(val[n])}}_{t}\\left(\\%,\\phi=$(ϕ_FTPL*ind_Taylor)\\right)\$"),
+            legendfontsize = 7,
+            palette        = palette([:blue,:red],N),
+            legend         = :outertopright)
     savefig(p,"theta_cum_$(val[n])_$(T_range[1])_FTPL.svg")
     display(p)
 end
